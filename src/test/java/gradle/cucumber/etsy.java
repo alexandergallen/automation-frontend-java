54c68589672bc445120943d61f2f96e99411c978
Alexander Gallen <alexandergallen@hotmail.com>
2020-02-09 23:20:43 +0100

    Added language en US since GB had different wording

--- src/test/java/gradle/cucumber/etsy.java
+++ src/test/java/gradle/cucumber/etsy.java
@@ -60,13 +60,13 @@ public class etsy {
                     "start-maximized",
                     "--ignore-certificate-errors",
                     "--no-sandbox",
-                    "lang=en-us",
+                    "--lang=en-US",
                     "--disable-infobars",
                     "--disable-browser-side-navigation",
                     "--disable-gpu",
                     "enable-automation");
         } else{
-            options.addArguments("--headless", "lang=en-us", "--window-size=1920,1080", "--start-maximized", "--disable-gpu");
+            options.addArguments("--headless", "--lang=en-US", "--window-size=1920,1080", "--start-maximized", "--disable-gpu");
         }
         System.setProperty("webdriver.chrome.driver", chromeDriverPath);
         driver = new ChromeDriver(options);
